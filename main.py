d3d5fdb84ee5836ea0ae897e9e89c53960a74d9c
Archer-SN <superkakanoob@gmail.com>
2021-10-30 16:38:49 +0700

    -Improved Checkmate system -Fixed Pawn bugs -Added Stalemate system

--- main.py
+++ main.py
@@ -16,19 +16,6 @@ kings = {"White": ["K", "", "'", 1],
          "Black": ["K'", "'", "", -1]}
 
 
-def new_game(chess_board, current_plr):
-    chess_board = [["R", "N", "B", "Q", "K", "B", "N", "R"],
-                   ["P", "P", "P", "P", "P", "P", "P", "P"],
-                   ["-", "-", "-", "-", "-", "-", "-", "-"],
-                   ["-", "-", "-", "-", "-", "-", "-", "-"],
-                   ["-", "-", "-", "-", "-", "-", "-", "-"],
-                   ["-", "-", "-", "-", "-", "-", "-", "-"],
-                   ["P'", "P'", "P'", "P'", "P'", "P'", "P'", "P'"],
-                   ["R'", "N'", "B'", "Q'", "K'", "B'", "N'", "R'"]]
-    current_plr = "White"  # White always starts first
-    check = False
-
-
 def convert_to_index(move):
     move = move.lower()
     if len(move) != 4 or not re.search(r"[a-h][1-8][a-h][1-8]", move):
@@ -47,7 +34,7 @@ def out_of_range(pos):
 
 
 def on_same_side(piece1, piece2):
-    if len(piece1) == len(piece2):
+    if len(piece1) == len(piece2) and piece1 != "-" and piece2 != "-":
         return True
     return False
 
@@ -62,32 +49,35 @@ def get_king_pos(turn):
     return king_position
 
 
-def promote_pawn(pawn_y, pawn_x, end_y):
+def promote_pawn(pawn_y, pawn_x, end_y, ally):
     pawn_piece = board[pawn_y][pawn_x]
-    if (len(pawn_piece) == 1 and end_y != 7) or (len(pawn_piece) == 2 and end_y != 0):
+    if (len(pawn_piece) == 1 and end_y != 7) or (len(pawn_piece) == 2 and end_y != 0) or pawn_piece not in ["P", "P'"]:
         return
     possible_promotion = ("Q", "N", "R", "B")
-    promoted_piece = None
     while True:
         promoted_piece = str(input("Promote Pawn to Q N R B: ")).upper()
         if promoted_piece in possible_promotion:
             break
-    board[pawn_y][pawn_x] = promoted_piece
+    board[pawn_y][pawn_x] = promoted_piece + ally
 
 
-def check_pawn(start_y, start_x, end_y, end_x):
+def check_pawn(start_y, start_x, end_y, end_x, is_test):
     if len(board[start_y][start_x]) == 1:
         side = 1
-        default_pos = 1
+        default_pos = 1  # The position that the pawn must be in if it wants to move by 2 blocks.
+        ally = ""
     else:
         side = -1
         default_pos = 6
+        ally = "'"
 
     if start_y + side == end_y:
         if ((start_x - 1 == end_x or start_x + 1 == end_x) and board[end_y][end_x] != "-") or (start_x == end_x and board[end_y][end_x] == "-"):
-            promote_pawn(start_y, start_x, end_y)
             return True
-    elif start_y + side*2 == end_y and start_y == default_pos:
+        if not is_test:
+            promote_pawn(start_y, start_x, end_y, ally)
+        return True
+    elif start_y + side*2 == end_y and start_y == default_pos and end_x == start_x and board[end_y][end_x] == "-" and board[start_y+side][start_x] == "-":
         return True
     return False
 
@@ -99,7 +89,7 @@ def check_rook(start_y, start_x, end_y, end_x):
             direction = -1  # Set the direction to -1 so we can loop backward if the start_pos is more than end_pos.
         horizontal_line = ["W" for row in range(start_x + direction, end_x, direction) if
                            board[start_y][row] != "-"]
-        if len(horizontal_line) == 0:
+        if len(horizontal_line) == 0:  # If there is no piece between the start piece and end piece.
             return True
     elif start_y != end_y and start_x == end_x:  # Vertical Line
         direction = 1
@@ -107,7 +97,7 @@ def check_rook(start_y, start_x, end_y, end_x):
             direction = -1  # Set the direction to -1 so we can loop backward if the start_pos is more than end_pos.
         vertical_line = ["W" for row in range(start_y + direction, end_y, direction) if
                          board[row][start_x] != "-"]
-        if len(vertical_line) == 0:
+        if len(vertical_line) == 0:  # If there is no piece between the start piece and end piece.
             return True
     return False
 
@@ -134,9 +124,9 @@ def check_bishop(start_pos, end_pos):
                     if board[y][x] != "-":
                         illegal_moves.append("W")
                     if end_pos == (y, x):
-                        if len(illegal_moves) == 1 and board[end_pos[0]][end_pos[1]] != "-":  # Only valid when the end_pos is not "-"
+                        if len(illegal_moves) == 1 and board[end_pos[0]][end_pos[1]] != "-":  # If there is one piece and that one piece is the end_piece.
                             return True
-                        elif len(illegal_moves) == 0 and board[end_pos[0]][end_pos[1]] == "-":  # Only valid when the end_pos is "-"
+                        elif len(illegal_moves) == 0 and board[end_pos[0]][end_pos[1]] == "-":  # If there is no piece between the start piece and end piece.
                             return True
                 except IndexError:
                     break
@@ -162,7 +152,7 @@ def check_king(y, x, end_pos):
     return False
 
 
-def check_move(start_pos, end_pos, turn):
+def check_move(start_pos, end_pos, turn, is_test=False):
     start_y, start_x = start_pos[0], start_pos[1]
     end_y, end_x = end_pos[0], end_pos[1]
     start_piece = board[start_y][start_x]
@@ -175,41 +165,44 @@ def check_move(start_pos, end_pos, turn):
         return False
     elif start_piece == "-":
         return False
-    elif len(start_piece) == len(end_piece) and end_piece != "-":  # Trying to eat ally piece
+    elif (turn == "White" and len(start_piece) == 2) or (turn == "Black" and len(start_piece) == 1):
         return False
-    moves_checker = {"P": check_pawn(start_y, start_x, end_y, end_x),
+    elif on_same_side(start_piece, end_piece):  # Trying to eat ally piece
+        return False
+    moves_checker = {"P": check_pawn(start_y, start_x, end_y, end_x, is_test),
                      "R": check_rook(start_y, start_x, end_y, end_x),
                      "N": check_knight(start_y, start_x, end_pos),
                      "B": check_bishop(start_pos, end_pos),
                      "Q": check_queen(start_pos, end_pos),
                      "K": check_king(start_y, start_x, end_pos),
-                     "-": False
                      }
     return moves_checker[start_piece[0]]
+
+
+def redo_move(start_pos, end_pos, s_piece, e_piece):
+    board[start_pos[0]][start_pos[1]] = s_piece
+    board[end_pos[0]][end_pos[1]] = e_piece
 
 
 def make_move(move, turn):
     if not convert_to_index(move):
         return False
     start_pos, end_pos = convert_to_index(move)
-    s_piece = board[start_pos[0]][start_pos[1]]
-    if (len(s_piece) == 1 and turn == "Black") or (len(s_piece) == 2 and turn == "White"):  # You can't move the enemy's piece. That's illegal
-        print("Trying to move the enemy piece!")
-        return False
+    st_piece = board[start_pos[0]][start_pos[1]]
+    ed_piece = board[end_pos[0]][end_pos[1]]
     # If the move is valid, we will eat the ending piece and replace it with the starting piece
     if check_move(start_pos, end_pos, turn):
         board[end_pos[0]][end_pos[1]] = board[start_pos[0]][start_pos[1]]
         board[start_pos[0]][start_pos[1]] = "-"
+        if check_if_check(current_player):
+            redo_move(start_pos, end_pos, st_piece, ed_piece)
+            print("You can't put your king in danger!")
+            return False
         return True
     else:
         print("Invalid move!")
         print()
         return False
-
-
-def redo_move(start_pos, end_pos, s_piece, e_piece):
-    board[start_pos[0]][start_pos[1]] = s_piece
-    board[end_pos[0]][end_pos[1]] = e_piece
 
 
 def check_by_knight(king_pos, enemy):
@@ -275,7 +268,6 @@ def check_if_check(turn, king_pos=None):
 def check_if_blockable(turn, king_pos):
     checker_pos = checker[0]
     checker_y, checker_x = checker_pos[0], checker_pos[1]
-    checker_piece = board[checker_y][checker_x]
     king_y, king_x = king_pos[0], king_pos[1]
     y_turn, x_turn = 0, 0
     if checker_y < king_y:
@@ -286,23 +278,11 @@ def check_if_blockable(turn, king_pos):
         x_turn = 1
     elif checker_x > king_x:
         x_turn = -1
-    y = 0  # Referencing the value outside the loop so we can pass it into the function dictionary
-    x = 0  #
-    moves_checker = {"P": check_pawn(y, x, checker_y, checker_x),
-                     "R": check_rook(y, x, checker_y, checker_x),
-                     "N": check_knight(y, x, checker_pos),
-                     "B": check_bishop((y, x), checker_pos),
-                     "Q": check_queen((y, x), checker_pos),
-                     "K": check_king(y, x, checker_pos),
-                     "-": False
-                     }
 
     while checker_y != king_y or checker_x != king_x:
-        for row in board:
-            for element in row:
-                if on_same_side(element, board[checker_y][checker_x]) or element == "-":
-                    continue
-                if moves_checker[element[0]]:
+        for y, row in enumerate(board):
+            for x, element in enumerate(row):
+                if check_move((y, x), checker_pos, turn):
                     print(element)
                     return True
         checker_y += y_turn
@@ -323,6 +303,36 @@ def check_mate(turn):
             return False
     if check_if_blockable(turn, king_pos):
         return False
+    return True
+
+
+def stale_mate(turn):
+    remaining_pieces = 0
+    for row in board:
+        for piece in row:
+            if piece != "-":
+                remaining_pieces += 1
+    if remaining_pieces == 2:  # These 2 pieces are kings. Which means that it is a stalemate because king alone can't make a checkmate.
+        return True
+
+    # This one checks if there is a possible move. If no then stalemate.
+    for start_y in range(8):  # The board is always 8x8. That's why I use 8 instead of len(board).
+        for start_x in range(8):
+            for end_y in range(8):
+                for end_x in range(8):
+                    if board[start_y][start_x] == "-":  # Skip "-" to save time
+                        continue
+                    if check_move((start_y, start_x), (end_y, end_x), turn, True):
+                        st_piece = board[start_y][start_x]
+                        ed_piece = board[end_y][end_x]
+                        board[end_y][end_x] = board[start_y][start_x]
+                        board[start_y][start_x] = "-"
+                        if check_if_check(turn):
+                            redo_move((start_y, start_x), (end_y, end_x), st_piece, ed_piece)
+                            continue
+                        else:
+                            redo_move((start_y, start_x), (end_y, end_x), st_piece, ed_piece)
+                            return False
     return True
 
 
@@ -350,6 +360,9 @@ while True:
             print("Checkmate!")
             break
         print("Check!")
+    elif stale_mate(current_player):
+        print("Stalemate!")
+        break
     print(current_player + " Turn!")
 
     while True:
@@ -357,14 +370,7 @@ while True:
         if not convert_to_index(input_move):
             continue
         start_move, end_move = convert_to_index(input_move)
-        st_piece = board[start_move[0]][start_move[1]]
-        ed_piece = board[end_move[0]][end_move[1]]
 
         if make_move(input_move, current_player):
-            if check_if_check(current_player):
-                redo_move(start_move, end_move, st_piece, ed_piece)
-                print("You can't put your king in danger!")
-                continue
-            else:
-                break
+            break
     current_player = next_turn(current_player)
